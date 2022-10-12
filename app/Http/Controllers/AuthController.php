4d4fe8f75fe3908607a387f871840aa6a7abd355
Abdiel Flores <abdielfg@gmail.com>
2022-10-12 14:58:51 -0600

    Auth Controller terminado

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,22 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request as RequestGuzzle;
+use Illuminate\Auth\RequestGuard;
 
 class AuthController extends Controller
 {
-    public function login(){
+    public function login(Request $request){
         $client = new Client();
         $options = [
         'multipart' => [
             [
             'name' => 'email',
-            'contents' => 'abdiel2_19@alu.uabcs.mx'
-            // 'contents' => $request->email
+            //'contents' => 'abdiel2_19@alu.uabcs.mx'
+            'contents' => $request->email
             ],
             [
             'name' => 'password',
-            'contents' => '41htT%pYl0I6kZ'
-            //'contents' => $request->password
+            //'contents' => '41htT%pYl0I6kZ'
+            'contents' => $request->password
             ]
         ]];
         $request = new RequestGuzzle('POST', 'https://crud.jonathansoto.mx/api/login');
@@ -39,6 +40,28 @@ class AuthController extends Controller
         }else{
             return view('login')->with("Error","Datos incorrectos");
         }
+    }
 
+    public function logout(Request $request){
+        $client = new Client();
+        $options = [
+        'multipart' => [
+            [
+            'name' => 'email',
+            'contents' => $request->email
+            ]
+        ]];
+        $request = new RequestGuzzle('POST', 'https://crud.jonathansoto.mx/api/logout');
+        $response = $client->sendAsync($request, $options)->wait();
+
+        $response = json_decode($response->getBody()->getContents());
+
+        if(isset($response->code) && $response->code > 0){
+            session_destroy();
+
+            return view('welcome');
+        }else{
+            return view('welcome')->with("Error","Datos incorrectos");
+        }
     }
 }
