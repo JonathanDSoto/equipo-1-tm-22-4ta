d7a756c154f39d69c117885d8c265120a4ec87ec
Francisco Diaz <fj.dp21@gmail.com>
2022-10-12 00:16:20 -0500

    login incompleto y vista del home tambien incompleta

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,139 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+<head>
+
+	<?php include "../layouts/head.template.php"; ?>
+
+    <!-- nouisliderribute css -->
+    <link rel="stylesheet" href="{{asset('libs/nouislider/nouislider.min.css')}}">
+
+    <!-- gridjs css -->
+    <link rel="stylesheet" href="{{asset('libs/gridjs/theme/mermaid.min.css')}}">
+</head>
+
+<body>
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+
+    	<?php include "../layouts/nav.template.php"; ?>
+        
+        <!-- ========== App Menu ========== -->
+        <?php include "../layouts/sidebar.template.php"; ?>
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <?php include "../layouts/bread.template.php"; ?>
+
+            <!-- End Page-content -->
+
+            <div class="row"> 
+
+                <div class="col-xl-12 col-lg-12">
+                    <div>
+                        <div class="card">
+                            <div class="card-header border-0">
+                                <div class="row g-4">
+                                    
+                                    <div class="col-sm">
+                                        <div class="d-flex justify-content-sm-start">
+                                            <div class="search-box ms-2">
+                                                <input type="text" class="form-control" id="searchProductList" placeholder="Search Products...">
+                                                <i class="ri-search-line search-icon"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="col-sm-auto">
+                                        <div div class="d-flex justify-content-sm-end">
+                                            <a href="apps-ecommerce-add-product.html" class="btn btn-success" id="addproduct-btn"><i class="ri-add-line align-bottom me-1"></i> Add Product</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            
+                            <!-- end card header -->
+                            <div class="card-body">
+
+                                <div class="tab-content text-muted">
+                                    <div class="tab-pane active" id="productnav-all" role="tabpanel">
+                                        <div id="table-product-list-all" class="table-card gridjs-border-none"></div>
+                                    </div>
+                                    <!-- end tab pane -->
+
+                                    <div class="tab-pane" id="productnav-published" role="tabpanel">
+                                        <div id="table-product-list-published" class="table-card gridjs-border-none"></div>
+                                    </div>
+                                    <!-- end tab pane -->
+
+                                    <div class="tab-pane" id="productnav-draft" role="tabpanel">
+                                        <div class="py-4 text-center">
+                                            <lord-icon src="https://cdn.lordicon.com/msoeawqm.json" trigger="loop" colors="primary:#405189,secondary:#0ab39c" style="width:72px;height:72px">
+                                            </lord-icon>
+                                            <h5 class="mt-4">Sorry! No Result Found</h5>
+                                        </div>
+                                    </div>
+                                    <!-- end tab pane -->
+                                </div>
+                                <!-- end tab content -->
+
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
+                    </div>
+                </div>
+                <!-- end col -->
+            </div>
+            <!-- end row -->
+
+            <!-- End Page-content -->
+
+
+
+            <?php include "../layouts/footer.template.php"; ?>
+        </div>
+        <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
+
+
+
+    <!--start back-to-top-->
+    <button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
+        <i class="ri-arrow-up-line"></i>
+    </button>
+    <!--end back-to-top-->
+
+    <!--preloader-->
+    <div id="preloader">
+        <div id="status">
+            <div class="spinner-border text-primary avatar-sm" role="status">
+                <span class="visually-hidden">Loading...</span>
+            </div>
+        </div>
+    </div>
+ 
+
+    <?php include "../layouts/scripts.template.php"; ?>
+
+    <!-- nouisliderribute js -->
+    <script src="{{asset(('libs/nouislider/nouislider.min.js'))}}"></script>
+    <script src="{{asset('libs/wnumb/wNumb.min.js')}}"></script>
+
+    <!-- gridjs js -->
+    <script src="{{asset('libs/gridjs/gridjs.umd.js')}}"></script>
+    <script src="../../../../unpkg.com/gridjs%405.1.0/plugins/selection/dist/selection.umd.js"></script>
+    <!-- ecommerce product list -->
+    <script src="{{asset('js/pages/ecommerce-product-list.init.js')}}"></script>
+
+
+</body>
+
+
+</html> -->
