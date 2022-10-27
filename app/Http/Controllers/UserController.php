cd2af081987d623b43587d39604c8f96b95f9295
Abdiel Flores <abdielfg@gmail.com>
2022-10-26 20:51:13 -0600

    User Controller store update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,6 +65,103 @@ class UserController extends Controller
         }
     }
 
+    public function store(Request $request){
+        $client = new Client();
+        $headers = [
+            'Authorization' => 'Bearer '. session('token')
+        ];
+        $options = [
+        'multipart' => [
+            [
+            'name' => 'name',
+            'contents' => $request->name
+            ],
+            [
+            'name' => 'lastname',
+            'contents' => $request->lastname
+            ],
+            [
+            'name' => 'email',
+            'contents' => $request->email
+            ],
+            [
+            'name' => 'phone_number',
+            'contents' => $request->phone_number
+            ],
+            [
+            'name' => 'created_by',
+            'contents' => $request->created_by
+            ],
+            [
+            'name' => 'role',
+            'contents' => $request->role
+            ],
+            [
+            'name' => 'password',
+            'contents' => $request->password
+            ],
+            [
+            'name' => 'profile_photo_file',
+            'contents' => Utils::tryFopen('/C:/Users/jsoto/Downloads/avatar.jpg', 'r'),
+            'filename' => '/C:/Users/jsoto/Downloads/avatar.jpg',
+            'headers'  => [
+                'Content-Type' => '<Content-type header>'
+            ]
+            ]
+        ]];
+        $request = new RequestGuzzle('POST', 'https://crud.jonathansoto.mx/api/users', $headers);
+
+        try {
+            $response = $client->send($request, $options);
+            $response = json_decode($response->getBody()->getContents());
+
+            //return redirect(route('users.index'));
+            //return view('users',compact('response'));
+
+
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+            //return view('index')->with("Error","Datos incorrectos");
+        }
+
+    }
+
+    public function update(Request $request){
+        $headers = [
+            'Authorization' => 'Bearer '. session('token'),
+            'Content-Type' => 'application/x-www-form-urlencoded'
+          ];
+          $options = [
+          'form_params' => [
+            'name' => $request->name,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'phone_number' => $request->phone_number,
+            'created_by' => $request->created_by,
+            'role' => $request->role,
+            'password' => $request->password,
+            'id' => $request->id
+          ]];
+
+          try {
+            $response = $client->send($request, $options);
+            $response = json_decode($response->getBody()->getContents());
+
+            //return redirect(route('users.index'));
+            //return view('users',compact('response'));
+
+
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+            //return view('index')->with("Error","Datos incorrectos");
+        }
+
+    }
+
+
+
 
 
 }
