1db45d7cfd60707f8f0cee510c87fbb81b54661e
Abdiel Flores <abdielfg@gmail.com>
2022-10-29 15:05:00 -0600

    Order Controller

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Auth\RequestGuard;
+use GuzzleHttp\Psr7\Request as RequestGuzzle;
+use GuzzleHttp\Psr7\Utils;
+use GuzzleHttp\Client;
 
 class OrderController extends Controller
 {
@@ -16,14 +20,63 @@ class OrderController extends Controller
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function getAllOrders(){
+        $client = new Client();
+        $headers = [
+            'Authorization' => 'Bearer '. session('token')
+        ];
+        $request = new RequestGuzzle('GET', 'https://crud.jonathansoto.mx/api/orders', $headers);
+
+        try {
+            $response = $client->sendAsync($request)->wait();
+            $orders = json_decode($response->getBody()->getContents());
+            $orders = $orders->data;
+
+            return view('Orders.index',compact('orders'));
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+            //return view('index')->with("Error","Datos incorrectos");
+        }
+    }
+
+    public function getSpecificOrder($id){
+        $client = new Client();
+        $headers = [
+            'Authorization' => 'Bearer '. session('token')
+        ];
+        $request = new RequestGuzzle('GET', 'https://crud.jonathansoto.mx/api/orders/details/'.$id, $headers);
+
+        try {
+            $response = $client->sendAsync($request)->wait();
+            $order = json_decode($response->getBody()->getContents());
+            $order = $order->data;
+
+            return view('Orders.detailOrder',compact('order'));
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+            //return view('index')->with("Error","Datos incorrectos");
+        }
+    }
+
+    public function getSpecificOrderBetweenDates($date1, $date2){
+        $client = new Client();
+        $headers = [
+        'Authorization' => 'Bearer 665|cUviyAgM8QDBvMJVoLuv2zfXYWAUPuqb2qeKwXEk'
+        ];
+        $request = new RequestGuzzle('GET', 'https://crud.jonathansoto.mx/api/orders/'.$date1.'/'.$date2, $headers);
+        try {
+            $response = $client->sendAsync($request)->wait();
+            $orders = json_decode($response->getBody()->getContents());
+            $orders = $orders->data;
+
+            return view('Orders.index',compact('orders'));
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+            //return view('index')->with("Error","Datos incorrectos");
+        }
     }
 
     /**
@@ -32,53 +85,111 @@ class OrderController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request){
+        $client = new Client();
+        $headers = [
+            'Authorization' => 'Bearer '. session('token')
+        ];
+        $options = [
+            'multipart' => [
+                [
+                    'name' => 'folio',
+                    'contents' => $request->folio
+                ],
+                [
+                    'name' => 'total',
+                    'contents' => $request->total
+                ],
+                [
+                    'name' => 'is_paid',
+                    'contents' => $request->is_paid
+                ],
+                [
+                    'name' => 'client_id',
+                    'contents' =>$request->client_id
+                ],
+                [
+                    'name' => 'address_id',
+                    'contents' => $request->address_id
+                ],
+                [
+                    'name' => 'order_status_id',
+                    'contents' => $request->order_status_id
+                ],
+                [
+                    'name' => 'payment_type_id',
+                    'contents' => $request->payment_type_id
+                ],
+                [
+                    'name' => 'coupon_id',
+                    'contents' => $request->coupon_id
+                ],
+                [
+                    'name' => 'presentations[0][id]',
+                    'contents' => '1'
+                ],
+                [
+                    'name' => 'presentations[0][quantity]',
+                    'contents' => '2'
+                ],
+                [
+                    'name' => 'presentations[1][id]',
+                    'contents' => '2'
+                ],
+                [
+                    'name' => 'presentations[1][quantity]',
+                    'contents' => '2'
+                ]
+            ]
+        ];
+        $request = new RequestGuzzle('POST', 'https://crud.jonathansoto.mx/api/orders', $headers);
+        $res = $client->sendAsync($request, $options)->wait();
+        echo $res->getBody();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+    public function update(Request $request, $id){
+        $client = new Client();
+        $headers = [
+            'Authorization' => 'Bearer '. session('token'),
+            'Content-Type' => 'application/x-www-form-urlencoded'
+        ];
+        $options = [
+            'form_params' => [
+                'id' => $id,
+                'order_status_id' => $request->order_status_id
+            ]
+        ];
+        $request = new RequestGuzzle('PUT', 'https://crud.jonathansoto.mx/api/orders', $headers);
+        try {
+            $response = $client->send($request, $options);
+            $response = json_decode($response->getBody()->getContents());
+
+            return redirect()->back()->with('success', 'true');
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
+
+            return redirect()->back()->with('error', 'true');
+        }
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    public function delete($id){
+        $client = new Client();
+        $headers = [
+            'Authorization' => 'Bearer '. session('token')
+        ];
+        $request = new RequestGuzzle('DELETE', 'https://crud.jonathansoto.mx/api/orders/1', $headers);
+        try {
+            $response = $client->sendAsync($request)->wait();
+            $user = json_decode($response->getBody()->getContents());
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+            return redirect()->back()->with('success', 'true');
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString = $response->getBody()->getContents();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+            return redirect()->back()->with('error', 'true');
+        }
     }
 }
