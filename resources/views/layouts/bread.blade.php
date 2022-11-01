6e8dd1eb2e3ef0868a1901b8fe1f10f6d9ef6a98
Ian <109057188+ianrjdev2001@users.noreply.github.com>
2022-10-31 21:25:22 -0700

    modificando otra vez el bread

--- resources/views/layouts/bread.blade.php
+++ resources/views/layouts/bread.blade.php
@@ -9,15 +9,18 @@
 
                             <div class="page-title-right">
                                 <ol class="breadcrumb m-0">
-                                    <li class="breadcrumb-item"><a href="/products">Inicio</a></li>
+                                    <li class="breadcrumb-item"><a href="/products">Inicio</a>  ></li>             
                                     <?php $link = "" ?>
                                     @for($i = 1; $i <= count(Request::segments()); $i++)
                                         @if($i < count(Request::segments()) & $i > 0)
                                         <?php $link .= "/" . Request::segment($i); ?>
-                                        <li class="breadcrumb-item" ><a href="<?= $link ?>">{{ ucwords(str_replace('-',' ',Request::segment($i)))}}</a> </li>  >
+                                        <li class="breadcrumb-item" > <a href="<?= $link ?>">{{ ucwords(str_replace('-',' ',Request::segment($i)))}}</a> > </li>
                                         @else {{ucwords(str_replace('-',' ',Request::segment($i)))}}
                                         @endif
                                     @endfor
+                                </ol>
+                                <ol>
+                                    
                                 </ol>
                             </div>
 
